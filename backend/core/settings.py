50a46dd7c3df7cb27ad946aa0e5af0bc6ce800a8
Hans Matthew Del Mundo <hdmGOAT@users.noreply.github.com>
2025-01-27 07:34:27 +0800

    moved connection to posgresql

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -73,10 +73,16 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+from decouple import config
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': config('DATABASE_NAME'),
+        'USER': config('DATABASE_USER'),
+        'PASSWORD': config('DATABASE_PASSWORD'),
+        'HOST': config('DATABASE_HOST', default='localhost'),
+        'PORT': config('DATABASE_PORT', default='5432')
     }
 }
 
