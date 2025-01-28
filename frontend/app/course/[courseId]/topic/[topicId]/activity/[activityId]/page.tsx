93f5aa244f12b7f0bcfae3b83a7b2794a53e5a19
Hans Matthew Del Mundo <hdmGOAT@users.noreply.github.com>
2025-01-28 22:34:49 +0800

    changed page to resizable

--- frontend/app/course/[courseId]/topic/[topicId]/activity/[activityId]/page.tsx
+++ frontend/app/course/[courseId]/topic/[topicId]/activity/[activityId]/page.tsx
@@ -1,29 +1,38 @@
 import CodeEditor from "@/components/CodeEditor";
 import { Button } from "@/components/ui/button";
+import {
+  ResizablePanelGroup,
+  ResizablePanel,
+  ResizableHandle,
+} from "@/components/ui/resizable";
 import React from "react";
 
 const ActivityPage = () => {
   return (
-    <div className="app-wrapper flex flex-row h-screen w-full">
-      <div className="prompt-container flex flex-col flex-1 space-y-3 p-4">
-        <h1 className="font-bold text-3xl">asdfa </h1>
-        <div className="flex flex-col space-y-3">
-          <p>Lets begin with a simple print statement</p>
-          <p>asdfasfdasdfasdf</p>
-          <code className="bg-zinc-700 text-white">print(hello World)</code>
-        </div>
-      </div>
-      <div className="flex flex-1 flex-col">
-        <div className="flex flex-col flex-[1] ">
-          <div className="code-container flex flex-col flex-1 overflow-scroll ">
-            <CodeEditor />
+    <div className=" h-screen w-screen">
+      <ResizablePanelGroup direction="horizontal" className="w-full">
+        <ResizablePanel defaultSize={50}>
+          <div className="flex h-[200px] items-center justify-center p-6">
+            <span className="font-semibold">One</span>
           </div>
-          <div className="flex">
-            <Button className="w-full">Run</Button>
-          </div>
-        </div>
-        <div className="flex flex-[1] h-full">OUTPUT</div>
-      </div>
+        </ResizablePanel>
+        <ResizableHandle />
+        <ResizablePanel defaultSize={50}>
+          <ResizablePanelGroup direction="vertical">
+            <ResizablePanel defaultSize={25}>
+              <div className="flex h-full overflow-y-scroll">
+                <CodeEditor />
+              </div>
+            </ResizablePanel>
+            <ResizableHandle />
+            <ResizablePanel defaultSize={75}>
+              <div className="p-6">
+                <span className="font-semibold">Output..</span>
+              </div>
+            </ResizablePanel>
+          </ResizablePanelGroup>
+        </ResizablePanel>
+      </ResizablePanelGroup>
     </div>
   );
 };
