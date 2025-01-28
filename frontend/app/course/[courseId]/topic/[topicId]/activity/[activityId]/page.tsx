d422cd4d17cb5663553c42e981deb42f93597468
Hans Matthew Del Mundo <hdmGOAT@users.noreply.github.com>
2025-01-28 22:56:01 +0800

    getting ready for running

--- frontend/app/course/[courseId]/topic/[topicId]/activity/[activityId]/page.tsx
+++ frontend/app/course/[courseId]/topic/[topicId]/activity/[activityId]/page.tsx
@@ -12,16 +12,40 @@ const ActivityPage = () => {
     <div className=" h-screen w-screen">
       <ResizablePanelGroup direction="horizontal" className="w-full">
         <ResizablePanel defaultSize={50}>
-          <div className="flex h-[200px] items-center justify-center p-6">
-            <span className="font-semibold">One</span>
+          <div className="flex p-6 flex-col space-y-4">
+            <h1 className="font-bold text-2xl">Welcome to Python!</h1>
+            <p>
+              PYTHON PYTHON PYTHON PYTHON PYTHON PYTHON PYTHON PYTHON PYTHON{" "}
+              PYTHON PYTHON PYTHON PYTHON PYTHON PYTHON PYTHON PYTHON PYTHON{" "}
+              PYTHON PYTHON PYTHON PYTHON PYTHON PYTHON PYTHON PYTHON PYTHON{" "}
+              PYTHON PYTHON PYTHON
+            </p>
+            <p>
+              PYTHON PYTHON PYTHON PYTHON PYTHON PYTHON PYTHON PYTHON PYTHON{" "}
+              PYTHON PYTHON PYTHON PYTHON PYTHON PYTHON PYTHON PYTHON PYTHON{" "}
+              PYTHON PYTHON PYTHON PYTHON PYTHON PYTHON PYTHON PYTHON PYTHON{" "}
+              PYTHON PYTHON PYTHON
+            </p>
+            <p>
+              PYTHON PYTHON PYTHON PYTHON PYTHON PYTHON PYTHON PYTHON PYTHON{" "}
+              PYTHON PYTHON PYTHON PYTHON PYTHON PYTHON PYTHON PYTHON PYTHON{" "}
+              PYTHON PYTHON PYTHON PYTHON PYTHON PYTHON PYTHON PYTHON PYTHON{" "}
+              PYTHON PYTHON PYTHON
+            </p>
           </div>
         </ResizablePanel>
         <ResizableHandle />
         <ResizablePanel defaultSize={50}>
           <ResizablePanelGroup direction="vertical">
             <ResizablePanel defaultSize={25}>
-              <div className="flex h-full overflow-y-scroll">
-                <CodeEditor />
+              <div className="flex h-full flex-col">
+                <div className="h-full flex overflow-y-scroll">
+                  <CodeEditor />
+                </div>
+                <div className="flex flex-row">
+                  <Button className="flex flex-[1]"> Run </Button>{" "}
+                  <Button className="flex flex-[2]">Submit</Button>
+                </div>
               </div>
             </ResizablePanel>
             <ResizableHandle />
--- frontend/components/CodeEditor.tsx
+++ frontend/components/CodeEditor.tsx
@@ -1,11 +1,10 @@
 "use client";
 
 import ReactCodeMirror, { EditorView } from "@uiw/react-codemirror";
-import { basicSetup } from "codemirror";
 import React, { useEffect, useRef, useState } from "react";
 import { vscodeDark } from "@uiw/codemirror-themes-all";
 import { loadLanguage } from "@uiw/codemirror-extensions-langs";
-import { Button } from "./ui/button";
+
 
 const CodeEditor = () => {
   const containerRef = useRef<HTMLDivElement>(null);
