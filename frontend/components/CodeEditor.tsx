3db5e572ee2d2b1f9f5def356209ffd4456f3fe6
Hans Matthew Del Mundo <hdmGOAT@users.noreply.github.com>
2025-01-28 22:46:14 +0800

    resolved text editor problem

--- frontend/components/CodeEditor.tsx
+++ frontend/components/CodeEditor.tsx
@@ -2,18 +2,40 @@
 
 import ReactCodeMirror, { EditorView } from "@uiw/react-codemirror";
 import { basicSetup } from "codemirror";
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { vscodeDark } from "@uiw/codemirror-themes-all";
 import { loadLanguage } from "@uiw/codemirror-extensions-langs";
 import { Button } from "./ui/button";
 
 const CodeEditor = () => {
+  const containerRef = useRef<HTMLDivElement>(null);
+  const [minHeight, setMinHeight] = useState(0);
+
+  useEffect(() => {
+    if (containerRef.current) {
+      setMinHeight(containerRef.current.offsetHeight);
+    }
+
+    const resizeObserver = new ResizeObserver(() => {
+      if (containerRef.current) {
+        setMinHeight(containerRef.current.offsetHeight);
+      }
+    });
+    if (containerRef.current) {
+      resizeObserver.observe(containerRef.current);
+    }
+
+    return () => resizeObserver.disconnect();
+  }, []);
+
   return (
-    <ReactCodeMirror
-      theme={vscodeDark}
-      extensions={[EditorView.lineWrapping, loadLanguage("python") || []]}
-      height="100vh"
-    />
+    <div ref={containerRef} style={{ width: "100%" }}>
+      <ReactCodeMirror
+        theme={vscodeDark}
+        extensions={[EditorView.lineWrapping, loadLanguage("python") || []]}
+        minHeight={`${minHeight}px`}
+      />
+    </div>
   );
 };
 
