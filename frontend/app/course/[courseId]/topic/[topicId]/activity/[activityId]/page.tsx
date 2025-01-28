ab724083d79437b1e28db9c07474585b35147413
Hans Matthew Del Mundo <hdmGOAT@users.noreply.github.com>
2025-01-28 22:00:43 +0800

    added run button

--- frontend/app/course/[courseId]/topic/[topicId]/activity/[activityId]/page.tsx
+++ frontend/app/course/[courseId]/topic/[topicId]/activity/[activityId]/page.tsx
@@ -1,4 +1,5 @@
 import CodeEditor from "@/components/CodeEditor";
+import { Button } from "@/components/ui/button";
 import ReactCodeMirror from "@uiw/react-codemirror";
 import React from "react";
 
@@ -6,22 +7,20 @@ const ActivityPage = () => {
   return (
     <div className="app-wrapper flex flex-row h-screen w-full">
       <div className="prompt-container flex flex-col flex-1 space-y-3 p-4">
-        <h1 className="font-bold text-3xl">Welcome to Python! </h1>
+        <h1 className="font-bold text-3xl">asdfa </h1>
         <div className="flex flex-col space-y-3">
           <p>Lets begin with a simple print statement</p>
-          <p>
-            In Python, the print() function is used to display output on the
-            screen. By writing print("Hello, World!"), the program instructs the
-            computer to output the text "Hello, World!" exactly as written. This
-            small yet foundational exercise helps new programmers understand how
-            to run a Python script and see immediate results, building
-            confidence for more complex programming tasks.
-          </p>
+          <p>asdfasfdasdfasdf</p>
           <code className="bg-zinc-700 text-white">print(hello World)</code>
         </div>
       </div>
-      <div className="code-container flex flex-1 overflow-hidden ">
-        <CodeEditor />
+      <div className="flex flex-col flex-1">
+        <div className="code-container flex flex-col flex-1 overflow-hidden ">
+          <CodeEditor />
+        </div>
+        <div>
+          <Button className="w-full">Run</Button>
+        </div>
       </div>
     </div>
   );
--- frontend/components/CodeEditor.tsx
+++ frontend/components/CodeEditor.tsx
@@ -5,15 +5,18 @@ import { basicSetup } from "codemirror";
 import React from "react";
 import { vscodeDark } from "@uiw/codemirror-themes-all";
 import { loadLanguage } from "@uiw/codemirror-extensions-langs";
+import { Button } from "./ui/button";
 
 const CodeEditor = () => {
   return (
-    <div className="w-full h-full">
-      <ReactCodeMirror
-        theme={vscodeDark}
-        extensions={[EditorView.lineWrapping, loadLanguage("python") || []]}
-        height="100vh"
-      />
+    <div>
+      <div className="w-full h-full">
+        <ReactCodeMirror
+          theme={vscodeDark}
+          extensions={[EditorView.lineWrapping, loadLanguage("python") || []]}
+          height="100vh"
+        />
+      </div>
     </div>
   );
 };
