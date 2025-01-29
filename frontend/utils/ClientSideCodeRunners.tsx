edb5230e942bc5a87395c753e86505784c81989a
Hans Matthew Del Mundo <hdmGOAT@users.noreply.github.com>
2025-01-29 09:03:49 +0800

    python runner

--- frontend/utils/ClientSideCodeRunners.tsx
+++ frontend/utils/ClientSideCodeRunners.tsx
@@ -1,5 +1,10 @@
 const runCode = async (code: string) => {
   try {
+    const pyodide = await import("pyodide").then((mod) => mod.loadPyodide());
+
+    const result = await pyodide.runPython(code);
+
+    return result;
   } catch (err) {
     console.error("Trouble running code: ", err);
   }
