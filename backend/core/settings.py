c9e60a5c2ce3eb8c4911ea2b59fab9549badb488
Hans Matthew Del Mundo <hdmGOAT@users.noreply.github.com>
2025-01-29 18:59:27 +0800

    configuring backend

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
+    # LMS Apps
+    'courses',    
+    # Administration Apps
 ]
 
 MIDDLEWARE = [
