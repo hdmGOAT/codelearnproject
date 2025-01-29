896fbf94748b849d66d24ac659ef2c8296d9317d
Hans Matthew Del Mundo <hdmGOAT@users.noreply.github.com>
2025-01-29 19:09:09 +0800

    working on models

--- backend/courses/models.py
+++ backend/courses/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Course(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    rating = models.FloatField()
+    completed_by = models.IntegerField()
+
+    def __str__(self):
+        return self.name
