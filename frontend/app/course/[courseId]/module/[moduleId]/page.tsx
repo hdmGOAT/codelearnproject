f3f35af0538a435ceee49faa55d67a6ca3a3aeeb
Hans Matthew Del Mundo <hdmGOAT@users.noreply.github.com>
2025-01-29 18:33:13 +0800

    starting backend work

--- frontend/app/course/[courseId]/module/[moduleId]/page.tsx
+++ frontend/app/course/[courseId]/module/[moduleId]/page.tsx
@@ -1,25 +1,25 @@
 export default function TopicPage({
   params,
 }: {
-  params: { courseId: string; topicId: string };
+  params: { courseId: string; moduleId: string };
 }) {
   return (
     <div>
-      <h1>Topic: {params.topicId}</h1>
+      <h1>Topic: {params.moduleId}</h1>
       <p>Course: {params.courseId}</p>
       <p>Select a lesson or activity to continue.</p>
       {/* Example navigation */}
       <ul>
         <li>
           <a
-            href={`/course/${params.courseId}/topic/${params.topicId}/lessons/1`}
+            href={`/course/${params.courseId}/topic/${params.moduleId}/lessons/1`}
           >
             Lesson 1
           </a>
         </li>
         <li>
           <a
-            href={`/course/${params.courseId}/topic/${params.topicId}/activity1`}
+            href={`/course/${params.courseId}/topic/${params.moduleId}/activity1`}
           >
             Activity 1
           </a>
