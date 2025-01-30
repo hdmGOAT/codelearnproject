f6aa265925856c58df2398e7889216099b3a56bd
Hans Matthew Del Mundo <hdmGOAT@users.noreply.github.com>
2025-01-30 09:41:31 +0800

    ModelViewSets for courses

--- backend/courses/views.py
+++ backend/courses/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Course, Module
+from .serializers import CourseSerializer, ModuleSerializer
 
 # Create your views here.
+class CourseViewSet(viewsets.ModelViewSet):
+    queryset = Course.objects.all()
+    serializer_class = CourseSerializer
+    
+class ModuleViewSet(viewsets.ModelViewSet):
+    queryset = Module.objects.all()
+    serializer_class = ModuleSerializer
