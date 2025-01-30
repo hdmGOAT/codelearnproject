d5156effb4f4ba00e0315312b2959138e2ff15ca
Hans Matthew Del Mundo <hdmGOAT@users.noreply.github.com>
2025-01-30 21:48:21 +0800

    added to urls

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import UserViewSet
+from .views import UserViewSet, UserRegistrationSet
 
 
 router = DefaultRouter()
@@ -8,4 +8,5 @@ router.register(r'users', UserViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('register/', UserRegistrationSet.as_view(), name='register'),
 ]
