177a370c02014065d4b8854c4713b17ba7d78739
Hans Matthew Del Mundo <hdmGOAT@users.noreply.github.com>
2025-01-30 09:35:57 +0800

    Question, Option serializers

--- backend/quizzes/serializers.py
+++ backend/quizzes/serializers.py
@@ -1,2 +1,13 @@
 from rest_framework import serializers
 from .models import Question , Option
+
+class QuestionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Question
+        fields = '__all__'
+
+class OptionSerializer(serializers.ModelSerializer):
+    question = serializers.PrimaryKeyRelatedField(queryset=Question.objects.all())
+    class Meta:
+        model = Option
+        fields = '__all__'
