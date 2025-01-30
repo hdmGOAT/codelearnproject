01cc5d80a8fb4fe40ba3a4b5a8b97f99514838f5
Hans Matthew Del Mundo <hdmGOAT@users.noreply.github.com>
2025-01-30 21:23:31 +0800

    disabled default create response for custom

--- backend/users/views.py
+++ backend/users/views.py
@@ -1,12 +1,17 @@
 from django.shortcuts import render
-from rest_framework import viewsets
+from rest_framework.viewsets import APIView, ModelViewSet
+from rest_framework.response import Response
+from rest_framework import status
 from .models import User
 from .serializers import UserSerializer
 
 # Create your views here.
 
-class UserViewSet (viewsets.ModelViewSet):
+class UserViewSet (ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+    
+    def create(self , request):
+        return Response({"error": "User creation is disabled"}, status=status.HTTP_405_METHOD_NOT_ALLOWED)
 
 
