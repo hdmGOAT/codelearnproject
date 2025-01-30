3b22a9c7654d2c64e932bf231ad2b4feef4576a3
Hans Matthew Del Mundo <hdmGOAT@users.noreply.github.com>
2025-01-30 09:32:06 +0800

    content sreializer

--- backend/content/serializers.py
+++ backend/content/serializers.py
@@ -2,6 +2,10 @@ from rest_framework import serializers
 from .models import Content
 
 class ContentSerializer(serializers.Serializer):
+    CONTENT_CHOICES = ["Quiz", "Lesson", "Activity"]
+    
+    content_type = serializers.ChoiceField(choices=CONTENT_CHOICES)
+    
     class Meta:
         model = Content
         fields = '__all__'
