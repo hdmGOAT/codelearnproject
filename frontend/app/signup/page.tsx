360dcd3d944951c0cd5364342411f3971a149742
Hans Matthew Del Mundo <hdmGOAT@users.noreply.github.com>
2025-01-31 22:09:52 +0800

    added form component

--- frontend/app/signup/page.tsx
+++ frontend/app/signup/page.tsx
@@ -1,7 +1,26 @@
+import { Form, FormField } from "@/components/ui/form";
 import React from "react";
 
 const SignupPage = () => {
-  return <div>SignupPage</div>;
+  return (<div>
+    <Form>
+  <FormField
+    control={...}
+    name="..."
+    render={() => (
+      <FormItem>
+        <FormLabel />
+        <FormControl>
+          { /* Your form field */}
+        </FormControl>
+        <FormDescription />
+        <FormMessage />
+      </FormItem>
+    )}
+  />
+</Form>
+
+  </div>);
 };
 
 export default SignupPage;
