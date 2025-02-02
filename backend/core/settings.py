8495f303541d77c01b53189eeefcf9a3ebb772e7
Hans Matthew Del Mundo <hdmGOAT@users.noreply.github.com>
2025-02-02 12:17:26 +0800

    jwt auth and refresh

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -150,3 +150,10 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+REST_AUTH = {
+    'USE_JWR': True,
+    'JWT_AUTH_COOKIE': 'jwt-auth',
+    'JWT_AUTH_REFRESH_COOKIE': 'jwt-refresh',
+}
