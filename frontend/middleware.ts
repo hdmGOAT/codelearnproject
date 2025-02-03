4c4224ddeff943c6dff3d2a96b311e8a0d86c227
Hans Matthew Del Mundo <hdmGOAT@users.noreply.github.com>
2025-02-03 22:26:29 +0800

    adding token verification to middleware

--- frontend/middleware.ts
+++ frontend/middleware.ts
@@ -1,6 +1,9 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
-import { middlewareRefresh } from "./lib/services/api/authService";
+import {
+  middlewareRefresh,
+  middlewareVerify,
+} from "./lib/services/api/authService";
 
 export default async function middleware(request: NextRequest) {
   let auth = request.cookies.get("jwt-auth");
@@ -8,16 +11,26 @@ export default async function middleware(request: NextRequest) {
 
   console.log("auth: ", auth, "\nrefresh: ", refresh);
 
-  if (auth === undefined && refresh !== undefined) {
-    const response = await middlewareRefresh(refresh.value);
-    if (response.ok) {
-      auth = request.cookies.get("jwt-auth");
+  if (auth === undefined) {
+    if (refresh !== undefined) {
+      const response = await middlewareRefresh(refresh.value);
+      if (response.ok) {
+        auth = request.cookies.get("jwt-auth");
+      } else {
+        console.error("error refreshing token: ", response);
+        return NextResponse.redirect(new URL("/login", request.url));
+      }
     } else {
-      console.error("error refreshing token: ", response);
+      //refresh is expired log in again is required
+      return NextResponse.redirect(new URL("/login", request.url));
     }
   }
 
-  return NextResponse.redirect(new URL("/login", request.url));
+  if (auth !== undefined) {
+    const response = await middlewareVerify(auth.value);
+  }
+
+  return NextResponse.next();
 }
 
 export const config = {
