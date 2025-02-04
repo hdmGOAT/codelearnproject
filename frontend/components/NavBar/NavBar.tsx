e955bdce8e66b9905738787e03751f413f4126d3
Hans Matthew Del Mundo <hdmGOAT@users.noreply.github.com>
2025-02-04 11:31:26 +0800

    this is cool

--- frontend/components/NavBar/NavBar.tsx
+++ frontend/components/NavBar/NavBar.tsx
@@ -18,6 +18,21 @@ const NavBar = () => {
         <Link href="/" className="text-lg font-bold tracking-wide">
           Open Code Learning
         </Link>
+
+        <NavigationMenu>
+          <NavigationMenuList>
+            <NavigationMenuItem>
+              <NavigationMenuTrigger>Learn</NavigationMenuTrigger>
+              <NavigationMenuContent>
+                <ul className="grid grid-cols-2 gap-4 w-full">
+                  <li>
+                    <NavigationMenuLink href="/learn">Learn</NavigationMenuLink>
+                  </li>
+                </ul>
+              </NavigationMenuContent>
+            </NavigationMenuItem>
+          </NavigationMenuList>
+        </NavigationMenu>
       </div>
     </nav>
   );
