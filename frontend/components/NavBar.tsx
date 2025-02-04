52a22b2df0e901b9fa85c1b188e37595dacb9acb
Hans Matthew Del Mundo <hdmGOAT@users.noreply.github.com>
2025-02-04 09:13:48 +0800

    syled navbar

--- frontend/components/NavBar.tsx
+++ frontend/components/NavBar.tsx
@@ -1,31 +1,44 @@
-import React from "react";
+import Link from "next/link";
 import {
   NavigationMenu,
-  NavigationMenuItem,
-  NavigationMenuLink,
   NavigationMenuList,
+  NavigationMenuItem,
   NavigationMenuTrigger,
   NavigationMenuContent,
-} from "./ui/navigation-menu";
+  NavigationMenuLink,
+} from "@/components/ui/navigation-menu";
 
 const NavBar = () => {
   return (
-    <NavigationMenu className="bg-foreground p-2 w-screen">
-      <NavigationMenuList className="flex justify-between">
-        <NavigationMenuItem>
-          <NavigationMenuTrigger>Home</NavigationMenuTrigger>
-          <NavigationMenuContent>
-            <NavigationMenuLink>Link</NavigationMenuLink>
-          </NavigationMenuContent>
-        </NavigationMenuItem>
-        <NavigationMenuItem>
-          <NavigationMenuTrigger>Home</NavigationMenuTrigger>
-        </NavigationMenuItem>
-        <NavigationMenuItem>
-          <NavigationMenuTrigger>Home</NavigationMenuTrigger>
-        </NavigationMenuItem>
-      </NavigationMenuList>
-    </NavigationMenu>
+    <nav className="bg-foreground text-foreground shadow-md">
+      <div className=" mx-auto flex justify-between items-center py-3 px-6">
+        <Link
+          href="/"
+          className="text-lg font-bold tracking-wide text-background"
+        >
+          Open Code Learning
+        </Link>
+
+        <NavigationMenu>
+          <NavigationMenuList className="flex space-x-6">
+            <NavigationMenuItem>
+              <NavigationMenuTrigger>Home</NavigationMenuTrigger>
+              <NavigationMenuContent className="bg-background shadow-md rounded-md">
+                <NavigationMenuLink className="block px-4 py-2 hover:bg-gray-100">
+                  <Link href="/">Dashboard</Link>
+                </NavigationMenuLink>
+              </NavigationMenuContent>
+            </NavigationMenuItem>
+            <NavigationMenuItem>
+              <NavigationMenuTrigger>About</NavigationMenuTrigger>
+            </NavigationMenuItem>
+            <NavigationMenuItem>
+              <NavigationMenuTrigger>Contact</NavigationMenuTrigger>
+            </NavigationMenuItem>
+          </NavigationMenuList>
+        </NavigationMenu>
+      </div>
+    </nav>
   );
 };
 
