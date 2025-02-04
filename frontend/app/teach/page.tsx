fe406b50cdfaea1a3b7e71ee4f17a000ecc39d1c
Hans Matthew Del Mundo <hdmGOAT@users.noreply.github.com>
2025-02-04 21:12:45 +0800

    working on teach page

---
+++ frontend/app/teach/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const TeachPage = () => {
+  return (
+    <div className="flex m-3 p-2 border-2 rounded-xl min-h-screen">
+      TeachPage
+    </div>
+  );
+};
+
+export default TeachPage;
--- frontend/components/NavBar/NavBar.tsx
+++ frontend/components/NavBar/NavBar.tsx
@@ -13,103 +13,35 @@ import {
 
 const NavBar = () => {
   return (
-    <nav className="bg-gray-900 text-white shadow-md">
-      <div className="mx-auto flex justify-between items-center py-3 px-6">
-        <Link href="/" className="text-lg font-bold tracking-wide w-full">
+    <nav className="bg-foreground text-background shadow-md flex items-center">
+      <div className="mx-auto flex justify-between py-3 px-6 w-full">
+        <Link
+          href="/"
+          className="text-lg font-bold tracking-wide align-middle h-full justify-center flex items-center"
+        >
           Open Code Learning
         </Link>
-
-        <div className="flex w-full">
-          <NavigationMenu>
-            <NavigationMenuList>
-              <NavigationMenuItem className="relative">
-                <NavigationMenuTrigger className="text-foreground">
-                  Learn
-                </NavigationMenuTrigger>
-                <NavigationMenuContent className="min-w-[200px] bg-gray-800 rounded-md shadow-lg p-2">
-                  <ul className="flex flex-col space-y-2">
-                    <li>
-                      <NavigationMenuLink
-                        href="/learn"
-                        className="block px-4 py-3 hover:bg-gray-700 rounded-md transition"
-                      >
-                        <h1 className="font-bold">Recommended</h1>
-                        <p className="text-xs text-gray-300">
-                          The best courses for you
-                        </p>
-                      </NavigationMenuLink>
-                    </li>
-                    <li>
-                      <NavigationMenuLink
-                        href="/learn"
-                        className="block px-4 py-3 hover:bg-gray-700 rounded-md transition"
-                      >
-                        <h1 className="font-bold">Explore</h1>
-                        <p className="text-xs text-gray-300">
-                          Dive into new waters of knowledge
-                        </p>
-                      </NavigationMenuLink>
-                    </li>
-                    <li>
-                      <NavigationMenuLink
-                        href="/learn"
-                        className="block px-4 py-3 hover:bg-gray-700 rounded-md transition"
-                      >
-                        <h1 className="font-bold">Following</h1>
-                        <p className="text-xs text-gray-300">
-                          Courses by those you follow
-                        </p>
-                      </NavigationMenuLink>
-                    </li>
-                  </ul>
-                </NavigationMenuContent>
-              </NavigationMenuItem>
-              <NavigationMenuItem className="relative">
-                <NavigationMenuTrigger className="text-foreground">
-                  Learn
-                </NavigationMenuTrigger>
-                <NavigationMenuContent className="min-w-[200px] bg-gray-800 rounded-md shadow-lg p-2">
-                  <ul className="flex flex-col space-y-2">
-                    <li>
-                      <NavigationMenuLink
-                        href="/learn"
-                        className="block px-4 py-3 hover:bg-gray-700 rounded-md transition"
-                      >
-                        <h1 className="font-bold">Recommended</h1>
-                        <p className="text-xs text-gray-300">
-                          The best courses for you
-                        </p>
-                      </NavigationMenuLink>
-                    </li>
-                    <li>
-                      <NavigationMenuLink
-                        href="/learn"
-                        className="block px-4 py-3 hover:bg-gray-700 rounded-md transition"
-                      >
-                        <h1 className="font-bold">Explore</h1>
-                        <p className="text-xs text-gray-300">
-                          Dive into new waters of knowledge
-                        </p>
-                      </NavigationMenuLink>
-                    </li>
-                    <li>
-                      <NavigationMenuLink
-                        href="/learn"
-                        className="block px-4 py-3 hover:bg-gray-700 rounded-md transition"
-                      >
-                        <h1 className="font-bold">Following</h1>
-                        <p className="text-xs text-gray-300">
-                          Courses by those you follow
-                        </p>
-                      </NavigationMenuLink>
-                    </li>
-                  </ul>
-                </NavigationMenuContent>
-              </NavigationMenuItem>
-            </NavigationMenuList>
-          </NavigationMenu>
-        </div>
       </div>
+
+      <NavigationMenu>
+        <NavigationMenuList>
+          <NavigationMenuItem>
+            <NavigationMenuTrigger className=" bg-foreground font-semibold text-xl">
+              <NavigationMenuLink href="/teach">Teach</NavigationMenuLink>
+            </NavigationMenuTrigger>
+          </NavigationMenuItem>
+          <NavigationMenuItem>
+            <NavigationMenuTrigger className=" bg- font-semibold text-xl">
+              <NavigationMenuLink href="/learn">Learn</NavigationMenuLink>
+            </NavigationMenuTrigger>
+          </NavigationMenuItem>
+          <NavigationMenuItem>
+            <NavigationMenuTrigger className=" bg- font-semibold text-xl">
+              <NavigationMenuLink href="/dashboard">User</NavigationMenuLink>
+            </NavigationMenuTrigger>
+          </NavigationMenuItem>
+        </NavigationMenuList>
+      </NavigationMenu>
     </nav>
   );
 };
