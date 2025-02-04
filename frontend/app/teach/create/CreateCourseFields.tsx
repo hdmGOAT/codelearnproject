a85c6a1ff67a04114d2e8bd608e130c0ba75a87c
Hans Matthew Del Mundo <hdmGOAT@users.noreply.github.com>
2025-02-04 21:42:41 +0800

    beginning fields

--- frontend/app/teach/create/CreateCourseFields.tsx
+++ frontend/app/teach/create/CreateCourseFields.tsx
@@ -1,1 +1,22 @@
-const CreateCourseFields = {};
+import { Step } from "@/types/dynamicFormTypes";
+
+export const CreateCourseFields: Step = {
+  title: "Create Course",
+  fields: [
+    {
+      name: "title",
+      label: "Course Title",
+      type: "text",
+    },
+    {
+      name: "description",
+      label: "Course Description",
+      type: "text",
+    },
+    {
+      name: "price",
+      label: "Course Price",
+      type: "number",
+    },
+  ],
+};
