fa2eac0d56f38a81f7eb7ed451716b5d32313369
Hans Matthew Del Mundo <hdmGOAT@users.noreply.github.com>
2025-02-04 21:19:24 +0800

    teach page

--- frontend/app/teach/page.tsx
+++ frontend/app/teach/page.tsx
@@ -2,8 +2,15 @@ import React from "react";
 
 const TeachPage = () => {
   return (
-    <div className="flex m-3 p-2 border-2 rounded-xl min-h-screen">
-      TeachPage
+    <div className="flex flex-col">
+      <div className="flex m-3 p-2 border-2 rounded-xl flex-col">
+        <h1 className="text-2xl font-semibold">My Courses</h1>
+        <p className="text-lg">Coming soon...</p>
+      </div>
+      <div className="flex m-3 p-2 border-2 rounded-xl flex-col">
+        <h1 className="text-2xl font-semibold">New Reviews</h1>
+        <p className="text-lg">Words from your community</p>
+      </div>
     </div>
   );
 };
