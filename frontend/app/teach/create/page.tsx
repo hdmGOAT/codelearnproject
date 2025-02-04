475301308e8510666e0d28227b489f66306e180e
Hans Matthew Del Mundo <hdmGOAT@users.noreply.github.com>
2025-02-05 07:35:58 +0800

    gonna work on toolbar

--- frontend/app/teach/create/page.tsx
+++ frontend/app/teach/create/page.tsx
@@ -3,7 +3,7 @@ import React from "react";
 
 const CreatePage = () => {
   return (
-    <div className="flex h-screen p-2 m-3 border-2 rounded-xl">
+    <div className="flex min-h-screen px-2 mx-3 mt-3 border-2 rounded-xl">
       <RichTextEditor />
     </div>
   );
--- frontend/components/RichTextEditor.tsx
+++ frontend/components/RichTextEditor.tsx
@@ -1,4 +1,5 @@
 "use client";
+
 import React from "react";
 import { useEditor, EditorContent } from "@tiptap/react";
 import StarterKit from "@tiptap/starter-kit";
@@ -7,11 +8,18 @@ const RichTextEditor = () => {
   const editor = useEditor({
     extensions: [StarterKit],
     content: `<p>Hello World! </p>`,
+    immediatelyRender: false,
+    editorProps: {
+      attributes: {
+        class: "border p-3 w-full min-h-64",
+      },
+    },
   });
 
   return (
-    <div className="editor border p-3">
-      <EditorContent editor={editor} />
+    <div className="editor w-full h-screen">
+      <div className="toolbar  w-full">Toolbar</div>
+      <EditorContent className=" size-full" editor={editor} />
     </div>
   );
 };
