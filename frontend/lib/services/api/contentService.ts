0bb30681c523411db1ab08ab5635b021eeb89100
Hans Matthew Del Mundo <hdmGOAT@users.noreply.github.com>
2025-02-07 21:42:28 +0800

    basic tag apis

--- frontend/lib/services/api/contentService.ts
+++ frontend/lib/services/api/contentService.ts
@@ -2,6 +2,42 @@ import apiClient from "./apiClient";
 
 // TAGS
 
+export const getTags = async () => {
+  try {
+    const response = await apiClient.get("/tags");
+    return response.data;
+  } catch (error) {
+    console.error("Error getting tags: ", error);
+  }
+};
+
+export const addTag = async (tag: string) => {
+  try {
+    const response = await apiClient.post("/tags", { tag });
+    return response.data;
+  } catch (error) {
+    console.error("Error adding tag: ", error);
+  }
+};
+
+export const deleteTag = async (tag: string) => {
+  try {
+    const response = await apiClient.delete(`/tags/${tag}`);
+    return response.data;
+  } catch (error) {
+    console.error("Error deleting tag: ", error);
+  }
+};
+
+export const updateTag = async (tag: string, newTag: string) => {
+  try {
+    const response = await apiClient.put(`/tags/${tag}`, { tag: newTag });
+    return response.data;
+  } catch (error) {
+    console.error("Error updating tag: ", error);
+  }
+};
+
 // COURSES
 
 // MODULES
