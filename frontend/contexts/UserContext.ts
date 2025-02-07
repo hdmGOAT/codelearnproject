bc8de9237c6cf38ca409ae84895ebb8280b806fd
Hans Matthew Del Mundo <hdmGOAT@users.noreply.github.com>
2025-02-07 23:04:48 +0800

    ill finish this tommorow

--- frontend/contexts/UserContext.ts
+++ frontend/contexts/UserContext.ts
@@ -1,1 +1,15 @@
 "use client";
+
+import { createContext, ReactNode, useContext, useState } from "react";
+
+type User = {
+  id: string;
+  email: string;
+  username: string;
+  profile: string;
+};
+
+type UserContextType = {
+  user: User | null;
+  setUser: (user: User) => void;
+};
