a1a68c356a26ba5c1ba23848df3d0783ba4f113f
Hans Matthew Del Mundo <hdmGOAT@users.noreply.github.com>
2025-03-17 19:51:25 +0800

    still exams

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -3,7 +3,7 @@ URL configuration for core project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.1/topics/http/urls/
-Examples:
+Examples:aaaaiaaio
 Function views
     1. Add an import:  from my_app import views
     2. Add a URL to urlpatterns:  path('', views.home, name='home')
